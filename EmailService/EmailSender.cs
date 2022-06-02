dc5862aa64717114e6cf6c9ac35ec6eed84b6d03
KCzerniak-pl <kczerniak@outlook.com>
2022-06-02 19:27:29 +0200

    Change Email Service.

--- EmailService/EmailSender.cs
+++ EmailService/EmailSender.cs
@@ -42,17 +42,20 @@ namespace EmailService
             {
                 try
                 {
-                    SecureSocketOptions secureSocketOptions = SecureSocketOptions.None;
-                    if (_emailConfig.SecureSocketOptions == 1) { secureSocketOptions = SecureSocketOptions.Auto; }
-                    else if (_emailConfig.SecureSocketOptions == 2) { secureSocketOptions = SecureSocketOptions.SslOnConnect; }
-                    else if (_emailConfig.SecureSocketOptions == 3) { secureSocketOptions = SecureSocketOptions.StartTls; }
-                    else if (_emailConfig.SecureSocketOptions == 4) { secureSocketOptions = SecureSocketOptions.StartTlsWhenAvailable; }
+                    if (!string.IsNullOrEmpty(_emailConfig.SmtpServer) || !string.IsNullOrEmpty(_emailConfig.UserName) || !string.IsNullOrEmpty(_emailConfig.Password))
+                    {
+                        SecureSocketOptions secureSocketOptions = SecureSocketOptions.None;
+                        if (_emailConfig.SecureSocketOptions == 1) { secureSocketOptions = SecureSocketOptions.Auto; }
+                        else if (_emailConfig.SecureSocketOptions == 2) { secureSocketOptions = SecureSocketOptions.SslOnConnect; }
+                        else if (_emailConfig.SecureSocketOptions == 3) { secureSocketOptions = SecureSocketOptions.StartTls; }
+                        else if (_emailConfig.SecureSocketOptions == 4) { secureSocketOptions = SecureSocketOptions.StartTlsWhenAvailable; }
 
-                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, secureSocketOptions);
-                    client.AuthenticationMechanisms.Remove("XOAUTH2");
-                    client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+                        client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, secureSocketOptions);
+                        client.AuthenticationMechanisms.Remove("XOAUTH2");
+                        client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
 
-                    client.Send(emailMessage);
+                        client.Send(emailMessage);
+                    }
                 }
                 catch
                 {
@@ -72,11 +75,14 @@ namespace EmailService
             {
                 try
                 {
-                    await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, true);
-                    client.AuthenticationMechanisms.Remove("XOAUTH2");
-                    await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
+                    if (!string.IsNullOrEmpty(_emailConfig.SmtpServer) || !string.IsNullOrEmpty(_emailConfig.UserName) || !string.IsNullOrEmpty(_emailConfig.Password))
+                    {
+                        await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                        client.AuthenticationMechanisms.Remove("XOAUTH2");
+                        await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
 
-                    await client.SendAsync(emailMessage);
+                        await client.SendAsync(emailMessage);
+                    }
                 }
                 catch
                 {
